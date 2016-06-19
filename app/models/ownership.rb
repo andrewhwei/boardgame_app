99cd63cf7b8b441e9fe8aee5ae9734587f192018
Andrew Wei <andrew.wei92@yahoo.com>
2016-06-19 11:48:30 -0700

    Added validations for ownerships

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -2,6 +2,7 @@ class Ownership < ActiveRecord::Base
   belongs_to :boardgame
   belongs_to :user
 
-  validates :rating, :num_of_plays, presence: true
-
+  validates :user_id, :boardgame_id, :rating, :num_of_plays, presence: true
+  validates :rating, :num_of_plays, numericality: { only_integer: true }
+  validates_uniqueness_of :boardgame_id, scope: [:user_id]
 end
